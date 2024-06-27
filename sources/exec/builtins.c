330549fa5c29fbadcba2cbc4eff704510684430e
michIsMuch69 <115632651+michIsMuch69@users.noreply.github.com>
2024-06-27 08:49:26 +0200

    added test script for exit

--- sources/exec/builtins.c
+++ sources/exec/builtins.c
@@ -6,7 +6,7 @@
 /*   By: jean-micheldusserre <jean-micheldusserr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 08:39:26 by jedusser          #+#    #+#             */
-/*   Updated: 2024/06/26 15:58:52 by jean-michel      ###   ########.fr       */
+/*   Updated: 2024/06/26 17:49:21 by jean-michel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,33 @@
 // int ft_unset(char **args);
 // int ft_env(char **env);
 // int ft_exit(void);
+
 void ft_exit(char **args)
 {
     int status = 0;
     if (args[1])
-	{
-        status = atoi(args[1]);
+    {
+        char *endptr;
+        status = strtol(args[1], &endptr, 10);
+        if (*endptr != '\0')
+        {
+            ft_putstr_fd("exit: numeric argument required\n", 2);
+            status = 255;
+        }
+        else
+        {
+            status = status % 256;
+            if (status < 0)
+                status += 256;
+        }
     }
-	else
-	{
+    else
+    {
         status = last_exit_code;
     }
-	printf("Exited with status code : %d\n", status);
     exit(status);
 }
+
 
 
 int ft_pwd(void)
@@ -40,11 +53,11 @@ int ft_pwd(void)
     if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
         ft_printf("%s\n", cwd);
-        return (0); // Success
+        return (0); 
     }
 	else
 	{
-        handle_error("my pwd", errno);// sets the last exit code 
+        handle_error("my pwd", errno);//last exit code 
         return (1);
     }
 }
@@ -64,16 +77,25 @@ int ft_env(char **env)
 }
 int ft_cd(char **args)
 {
-	printf("My cd\n");
+    printf("My cd\n");
     if (!args[1])
-	{
-        handle_error("my cd ", errno);
-        return (1); 
+    {
+        const char *home = getenv("HOME");
+        if (home == NULL)
+		{
+            handle_error("my cd: HOME not set", errno);
+            return (1);
+        }
+        if (chdir(home) == -1)
+		{
+            handle_error("my cd: HOME", errno);
+            return (1);
+        }
     }
-	if (chdir(args[1]) == -1)
-	{
-		handle_error("my cd : args[1]", errno); 
-		return (1);
-	}
-    return (0); 
+    else if (chdir(args[1]) == -1)
+    {
+        handle_error("my cd: args[1]", errno);
+        return (1);
+    }
+    return (0);
 }
---
+++ test_bash_exit_cases.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# test_bash_exit_cases.sh
+
+echo "Testing Bash exit command:"
+
+bash << EOF
+exit 0
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit 1
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit 42
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit 255
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit abc
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit 256
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit -1
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+exit 1000
+EOF
+echo "Exit status: $?"
+
+bash << EOF
+pwd
+exit
+EOF
+echo "Exit status: $?"
