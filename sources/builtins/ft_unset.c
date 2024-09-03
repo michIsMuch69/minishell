c4701854ab8a08d38d21f8bcd2183d6fb21b7315
Jean-Michel Dusserre <jedusser@z3r9p1.42lyon.fr>
2024-09-03 08:48:27 +0200

    refacto unset

--- sources/builtins/ft_unset.c
+++ sources/builtins/ft_unset.c
@@ -6,11 +6,23 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 08:27:25 by jedusser          #+#    #+#             */
-/*   Updated: 2024/09/02 16:01:57 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/09/03 08:33:06 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+void	remove_env_var(t_data *data, int j)
+{
+	free(data->env.tab[j]);
+	while (j < data->env.size - 1)
+	{
+		data->env.tab[j] = data->env.tab[j + 1];
+		j++;
+	}
+	data->env.size--;
+	data->env.tab[data->env.size] = NULL;
+}
 
 void	ft_unset_env(t_data *data)
 {
@@ -26,14 +38,7 @@ void	ft_unset_env(t_data *data)
 			if (ft_strncmp(data->env.tab[j], data->args.tab[i],
 					ft_strlen(data->args.tab[i])) == 0)
 			{
-				free(data->env.tab[j]);
-				while (j < data->env.size - 1)
-				{
-					data->env.tab[j] = data->env.tab[j + 1];
-					j++;
-				}
-				data->env.size--;
-				data->env.tab[data->env.size] = NULL;
+				remove_env_var(data, j);
 				break ;
 			}
 			j++;
@@ -42,34 +47,39 @@ void	ft_unset_env(t_data *data)
 	}
 }
 
-void ft_unset_export(t_data *data)
+void	remove_export_var(t_data *data, int j)
 {
-    int	i;
-    int	j;
+	free(data->export.tab[j]);
+	while (j < data->export.size - 1)
+	{
+		data->export.tab[j] = data->export.tab[j + 1];
+		j++;
+	}
+	data->export.size--;
+	data->export.tab[data->export.size] = NULL;
+}
 
-    i = 0;
-    while (i < data->args.size)
-    {
-        j = 0;
-        while (j < data->export.size)
-        {
-            if (ft_strncmp(data->export.tab[j], data->args.tab[i],
-                    ft_strlen(data->args.tab[i])) == 0)
-            {
-                free(data->export.tab[j]);
-                while (j < data->export.size - 1)
-                {
-                    data->export.tab[j] = data->export.tab[j + 1];
-                    j++;
-                }
-                data->export.size--;
-                data->export.tab[data->export.size] = NULL;
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
+void	ft_unset_export(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < data->args.size)
+	{
+		j = 0;
+		while (j < data->export.size)
+		{
+			if (ft_strncmp(data->export.tab[j], data->args.tab[i],
+					ft_strlen(data->args.tab[i])) == 0)
+			{
+				remove_export_var(data, j);
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 int	ft_unset(t_data *data, int i)
